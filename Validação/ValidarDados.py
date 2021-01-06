4f09ae3a449bed922094ee663268d6493a6f3ac3
RDA <ricardo.dias.andrade@gmail.com>
2021-01-06 23:59:17 +0000

    comit

---
+++ Validação/ValidarDados.py
@@ -0,0 +1,23 @@
+import os, os.path
+from os import path
+
+
+def Checkfile(fileName):
+
+    if(path.exists(fileName)):
+
+        if(os.stat(fileName).st_size == 0):
+            f = open(fileName,"w")
+            f.write("Nome-Email-Telefone\n")
+            f.close()
+        #endif
+
+    else:
+        f = open(fileName,"w")
+        f.write("Nome-Email-Telefone\n")
+        f.close()
+    #endif
+
+    return True
+        
+            
