c1b94b9c099214c37de1441cce0aa062e10cd6b0
RDA <ricardo.dias.andrade@gmail.com>
2021-01-11 00:50:08 +0000

    commit

--- Validacao/ValidarDados.py
+++ Validacao/ValidarDados.py
@@ -145,7 +145,7 @@ def CheckfileTw(fileName):
             return False
         
         else:
-            f= open(fileName,'r')
+            f= open(fileName,'r')#use this code in insert to know the id number => total number of lines - 1(firstline)
             ln = f.readlines()
             f.close()
 
